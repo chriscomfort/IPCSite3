7f139373f809fb499233cf1eecf0e77f10af9f17
Chris Comfort <chriscomfort@Chriss-iMac-Pro.lan>
2023-08-15 12:03:19 -0400

    fix spacing on about/contact and remove phone from btk

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var pastWork = document.getElementById("past-work");
+  var subnav = document.querySelector(".subnav");
+  var subnavLinks = document.querySelectorAll(".subnav a"); // Select subnav links
+
+  pastWork.addEventListener("click", function(event) {
+    event.preventDefault();
+    subnav.classList.toggle("active");
+  });
+
+
+  subnavLinks.forEach(function(link) {
+    link.addEventListener("click", function(event) {
+      event.preventDefault();
+      var targetPage = link.getAttribute("href");
+      window.location.href = targetPage;
+    });
+  });
+});
